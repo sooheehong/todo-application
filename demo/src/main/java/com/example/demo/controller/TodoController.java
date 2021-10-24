00167a35c481cb0d1a1a2fe80332f23355894c20
sooheehong <sooheehong@kakao.com>
2021-10-24 18:41:00 +0900

    2.2.4 컨트롤러 레이어 - 스프링 REST API 컨트롤러

---
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+
+	@GetMapping("/testTodo")
+	public ResponseEntity<?> testControllerResponseEntityWithTodo() {
+		List<String> list = new ArrayList();
+		list.add("Hello World~~ I'm ResponseEntityWithTodo. And you got 200!");
+		
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
+	}
+	
+}
