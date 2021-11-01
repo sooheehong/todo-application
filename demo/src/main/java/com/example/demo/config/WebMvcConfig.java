aa87653c5930a06111bc41f04de2f2acaccd4a9f
sooheehong <sooheehong@kakao.com>
2021-11-01 22:29:27 +0900

    sencha app port 추가

--- demo/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("http://localhost:3000", "http://localhost:1841")
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCDJ", "OPTIONS")
 		.allowedHeaders("*")
 		.allowCredentials(true)
