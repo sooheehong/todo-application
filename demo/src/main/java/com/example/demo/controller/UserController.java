4984c693fc43d88f038b7a7c4c1af7a24b5b2b0e
sooheehong <sooheehong@kakao.com>
2021-11-06 22:11:56 +0900

    회원가입 시 유저 패스워드 암호화 추가

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -45,7 +45,7 @@ public class UserController {
 			UserEntity user = UserEntity.builder()
 					.email(userDTO.getEmail())
 					.username(userDTO.getUsername())
-					.password(userDTO.getPassword())
+					.password(passwordEncoder.encode(userDTO.getPassword()))
 					.build();
 			
 			UserEntity registeredUser = userService.create(user);
